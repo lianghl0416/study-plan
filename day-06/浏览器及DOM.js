daf1c4385e501c46fec32e32a8d734fafe327529
lianghl0416 <594207356@qq.com>
2020-04-10 20:20:25 +0800

    操作DOM

---
+++ day-06/浏览器及DOM.js
@@ -0,0 +1,22 @@
+// var
+// js = document.getElementById('js'),
+// list = document.getElementById('list');
+// list.appendChild(js);
+// 
+var
+    list = document.getElementById('list'),
+    ref = document.getElementById('python'),
+    haskell = document.createElement('p');
+haskell.id = 'haskell';
+haskell.innerText = 'Haskell';
+list.insertBefore(haskell, ref);
+
+var
+    i, c,
+    list = document.getElementById('list');
+for (i = 0; i < list.children.length; i++) {
+    c = list.children[i]; // 拿到第i个子节点
+}
+
+
+
