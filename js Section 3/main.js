eb505646643218b085e07c96edd981e8b0740fe3
lianghl0416 <594207356@qq.com>
2020-04-19 18:28:34 +0800

    js scetion 3

---
+++ js Section 3/main.js
@@ -0,0 +1,10 @@
+var div = document.getElementById('canvas')
+div.onmousedown = function (a) {
+    var x = a.clientx
+    var y = a.clienty
+}
+div.onmousemove = function () {
+}
+div.onmouseup = function (z) {
+    console.log(1)
+}
