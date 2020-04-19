556738ed75ad97039c7de197555312463cd527bc
lianghl0416 <594207356@qq.com>
2020-04-19 19:08:19 +0800

    mouse事件

--- js Section 3/main.js
+++ js Section 3/main.js
@@ -2,6 +2,9 @@ var div = document.getElementById('canvas')
 div.onmousedown = function (a) {
     var x = a.clientx
     var y = a.clienty
+    var divA = document.createElement('div')
+    divA.style = "width:6px; height:6px; background:black; border-radius:3px; position:absolute; left: " + (x-3) + "px" + "top: " + (y-3) + "px"
+    div.appendChild(divA)
 }
 div.onmousemove = function () {
 }
