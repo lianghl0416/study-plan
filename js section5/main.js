b62d7bb149a30c04dc5ad061775a2f323ddd542a
lianghl0416 <594207356@qq.com>
2020-04-24 18:40:33 +0800

    添加颜色

--- js section5/main.js
+++ js section5/main.js
@@ -17,6 +17,39 @@ autoSetCanvasSize(yyy)
 listenToUser(yyy)
 //控制橡皮檫是否开启：
 var eraserEnabled = false
+brush.onclick = function () {
+    eraserEnabled = false
+    brush.classList.add('active')
+    eraser.classList.remove('active')
+}
+eraser.onclick = function () {
+    eraserEnabled = true
+    eraser.classList.add('active')
+    brush.classList.remove('active')
+}
+
+red.onclick = function () {
+    context.fillStyle = 'red'
+    context.strokeStyle = 'red'
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+green.onclick = function () {
+    context.fillStyle = 'green'
+    context.strokeStyle = 'green'
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+}
+blue.onclick = function () {
+    context.fillStyle = 'blue'
+    context.strokeStyle = 'blue'
+    blue.classList.add('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+}
+
 
 //控制画板宽高属性
 function autoSetCanvasSize(canvas) {
@@ -33,34 +66,18 @@ function autoSetCanvasSize(canvas) {
 }
 function drawCircle(x, y, radius) {
     context.beginPath();
-    context.fillStyle = "black"
     context.arc(x, y, radius, 0, Math.PI * 2);
     context.fill()
 }
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
-    context.strokeStyle = "black"
     context.moveTo(x1, y1)//起点
     context.lineWidth = 3
     context.lineTo(x2, y2)//终点
     context.stroke()
     context.closePath()
 }
-function drawCircle(x, y, radius) {
-    context.beginPath();
-    context.fillStyle = "black"
-    context.arc(x, y, radius, 0, Math.PI * 2);
-    context.fill()
-}
-function drawLine(x1, y1, x2, y2) {
-    context.beginPath();
-    context.strokeStyle = "black"
-    context.moveTo(x1, y1)//起点
-    context.lineWidth = 3
-    context.lineTo(x2, y2)//终点
-    context.stroke()
-    context.closePath()
-}
+
 function listenToUser(canvas) {
     //曲线，监听鼠标事件：
     var using = false
