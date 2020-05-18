ac7ee7a91af358be1c7c3b52786aad0d47236f3c
lianghl0416 <594207356@qq.com>
2020-05-18 21:11:42 +0800

    修改Node.prototype

--- day-11/11.js
+++ day-11/11.js
@@ -1,15 +1,16 @@
-function getSibling(node) {
-    var allChildren = item3.parentNode.children
-    var array = { length: 0 }
-    for (let i = 0; i < allChildren.length; i++) {
-        if (allChildren[i] !== node) {
-            array[array.length] = allChildren[i]
-            array.length += 1
-        }
-    }
-    return array
-}
-
+//封装一个函数
+// function getSibling(node) {
+    // var allChildren = node.parentNode.children
+    // var array = { length: 0 }
+    // for (let i = 0; i < allChildren.length; i++) {
+        // if (allChildren[i] !== node) {
+            // array[array.length] = allChildren[i]
+            // array.length += 1
+        // }
+    // }
+    // return array
+// }
+//封装第二个函数
 function addclass(node, classes) {
 
     for (let key in classes) {
@@ -18,10 +19,39 @@ function addclass(node, classes) {
         node.classList[methodName](key)
     }
 }
+//命名空间
+// window.lhldom = {}
+// lhldom.getSibling = getSibling
+// lhldom.addclass = addclass
+// 
+// lhldom.getSibling(item5)
 
-window.lhldom = {}
-lhldom.getSibling = getSibling
-lhldom.addclass = addclass
+//
+Node.prototype.getSibling = function() {
+    var allChildren = this.parentNode.children
+    var array = { length: 0 }
+    for (let i = 0; i < allChildren.length; i++) {
+        if (allChildren[i] !== this) {
+            array[array.length] = allChildren[i]
+            array.length += 1
+        }
+    }
+    return array
+}
 
-lhldom.getSibling(item5)
+item4.getSibling()
+item4.getSibling.call(item4)
 
+
+Node.prototype.addclass = function (classes) {
+
+    for (let key in classes) {
+        var value = classes[key]
+        var methodName = value ? 'add' : 'remove'
+        this.classList[methodName](key)
+    }
+}
+
+item4.addclass({'a':true,'b':false})
+item4.addclass.call(item4,{'a':true,'b':false})
+ 
