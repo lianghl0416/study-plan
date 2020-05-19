71a2725a26ce84b890e4f97eaa0684cbb5497190
lianghl0416 <594207356@qq.com>
2020-05-19 17:54:17 +0800

    jquery addclass (function(index,currentclass) toggleclass

--- day-11/11.js
+++ day-11/11.js
@@ -57,43 +57,46 @@
 
 
 // jQuery的使用
-window.jQuery = function (nodeOrSelector) {
-    let nodes = {}
-    if (typeof nodeOrSelector === 'string') {
-        let temp = document.querySelectorAll(nodeOrSelector)  //伪数组
-        for (let i = 0; i < temp.length; i++) {
-            nodes[i] = temp[i]
-        }
-        nodes.length = temp.length
-    } else if (nodeOrSelector instanceof nodes) {
-        nodes = {
-            0: nodeOrSelector, length: 1
-        }
-    }
-    nodes.addClass = function (classes) {
-        classes.forEach((value) => {
-            for (let i = 0; i < nodes.length; i++) {
-                nodes[i].classList.add(value)
-            }
-        })
-    }
-    nodes.text = function (text) {
-        if (text === undefined) {
-            var texts = []
-            for (let i = 0; i < nodes.length; i++) {
-                texts.push(nodes[i].textContent)
-            }
-            return texts
-        } else {
-            for (let i = 0; i < nodes.length; i++) {
-                nodes[i].textContent = text
-            }
-        }
-    }
-    return nodes
-}
+// window.jQuery = function (nodeOrSelector) {
+    // let nodes = {}
+    // if (typeof nodeOrSelector === 'string') {
+        // let temp = document.querySelectorAll(nodeOrSelector)  //伪数组
+        // for (let i = 0; i < temp.length; i++) {
+            // nodes[i] = temp[i]
+        // }
+        // nodes.length = temp.length
+    // } else if (nodeOrSelector instanceof nodes) {
+        // nodes = {
+            // 0: nodeOrSelector, length: 1
+        // }
+    // }
+    // nodes.addClass = function (classes) {
+        // classes.forEach((value) => {
+            // for (let i = 0; i < nodes.length; i++) {
+                // nodes[i].classList.add(value)
+            // }
+        // })
+    // }
+    // nodes.text = function (text) {
+        // if (text === undefined) {
+            // var texts = []
+            // for (let i = 0; i < nodes.length; i++) {
+                // texts.push(nodes[i].textContent)
+            // }
+            // return texts
+        // } else {
+            // for (let i = 0; i < nodes.length; i++) {
+                // nodes[i].textContent = text
+            // }
+        // }
+    // }
+    // return nodes
+// }
+// 
+// 
+// var node2 = jQuery('ul>li')
+// node2.addClass(['blue'])
+// var text = node2.text()
 
 
-var node2 = jQuery('ul>li')
-node2.addClass(['blue'])
-var text = node2.text()
+
