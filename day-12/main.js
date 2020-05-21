c3557a7e4f3165c02fa0ff80f2435c33bb79b9d1
lianghl0416 <594207356@qq.com>
2020-05-21 11:47:02 +0800

    轮播初实现

--- day-12/main.js
+++ day-12/main.js
@@ -1,4 +1,15 @@
-p1.onclick = function(){
-    
-
-}
+$(p1).on('click' , function(){
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+$(p2).on('click' , function(){
+    $(images).css({
+        transform: 'translateX(-300px)'
+    })
+})
+$(p3).on('click' , function(){
+    $(images).css({
+        transform: 'translateX(-600px)'
+    })
+})
