803ee1f13de4f50e041d182ec5f971249d3e0851
lianghl0416 <594207356@qq.com>
2020-05-21 16:48:38 +0800

    轮播node  重构

--- day-12/main.js
+++ day-12/main.js
@@ -20,34 +20,44 @@ var allButtons = $('#buttons > button')
 for (let i = 0; i < allButtons.length; i++) {
     $(allButtons[i]).on('click', function (x) {
         var index = $(x.currentTarget).index()
-        var n = index * -300
+        var p = index * -300
         $('#images').css({
-            transform: 'translate(' + n + 'px )'
+            transform: 'translate(' + p + 'px )'
         })
+        n = index
+        activeButton(allButtons.eq(n))
     })
 }
 //添加自动滚动，且滚动到的标签高亮
 var n = 0;
 var size = allButtons.length
-allButtons.eq(n % size).trigger('click')
-    .addClass('red')
-    .siblings('.red').removeClass('red')
-var timerId = setInterval(() => {
-    n += 1
-    allButtons.eq(n % size).trigger('click')
+playSlide(n % size)
+
+var timerId = setTimer()
+
+function setTimer(){
+    return setInterval(() => {
+        n += 1
+        playSlide(n % size)
+    }, 2000)
+}
+
+function playSlide(index) {
+    allButtons.eq(index).trigger('click')
+}
+
+function activeButton($button) {
+    $button
         .addClass('red')
         .siblings('.red').removeClass('red')
-}, 2000)
+}
+
 //鼠标移入停止轮播
 $('.window').on('mouseenter', function () {
     window.clearInterval(timerId)
 })
+
 //鼠标移出 ，重新轮播
 $('.window').on('mouseleave', function () {
-    timerId = setInterval(() => {
-        n += 1
-        allButtons.eq(n % size).trigger('click')
-            .addClass('red')
-            .siblings('.red').removeClass('red')
-    }, 2000)
+    timerId = setTimer()
 })
