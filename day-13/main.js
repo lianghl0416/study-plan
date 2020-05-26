1ed1300d43efa038a7af184437e600a7f9949748
lianghl0416 <594207356@qq.com>
2020-05-26 16:53:05 +0800

    无缝轮播插入jquery

---
+++ day-13/main.js
@@ -0,0 +1,66 @@
+// setTimeout(function () {
+// $('.images>img:nth-child(1)').css({
+// transform: 'translateX(-100%)'
+// })
+// $('.images>img:nth-child(2)').css({
+// transform: 'translateX(-100%)'
+// })
+// $('.images>img:nth-child(1)').one('transitionend',function(e){
+// $(e.currentTarget) .addClass('right').css({transform:'none'})
+// 
+// })
+// }, 2000)
+// setTimeout(function () {
+// $('.images>img:nth-child(2)').css({
+// transform: 'translateX(-200%)'
+// })
+// $('.images>img:nth-child(3)').css({
+// transform: 'translateX(-100%)'
+// })
+// $('.images>img:nth-child(2)').one('transitionend',function(e){
+// $(e.currentTarget) .addClass('right').css({transform:'none'})
+// })
+// }, 4000)
+// setTimeout(function () {
+// $('.images>img:nth-child(3)').css({
+// transform: 'translateX(-200%)'
+// })
+// $('.images>img:nth-child(1)').css({
+// transform: 'translateX(-100%)'
+// })
+// $('.images>img:nth-child(3)').one('transitionend',function(e){
+// $(e.currentTarget) .addClass('right').css({transform:'none'})
+// })
+// }, 6000)
+// setTimeout(function () {
+// $('.images>img:nth-child(1)').css({
+// transform: 'translateX(-200%)'
+// })
+// $('.images>img:nth-child(2)').css({
+// transform: 'translateX(-100%)'
+// })
+// $('.images>img:nth-child(1)').one('transitionend',function(e){
+// $(e.currentTarget) .addClass('right').css({transform:'none'})
+// 
+// })
+// }, 8000)
+
+
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+setTimeout(() => {
+    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
+        })
+
+    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+
+
+}, 2000)
+
+
+
+
+
+
