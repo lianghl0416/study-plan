a931cb35e45e1e3a00330440571e8bc03b1f6a1a
lianghl0416 <594207356@qq.com>
2020-05-26 17:11:25 +0800

    实现轮播

--- day-13/main.js
+++ day-13/main.js
@@ -48,27 +48,54 @@
 
 $('.images > img:nth-child(1)').addClass('current')
 $('.images > img:nth-child(2)').addClass('enter')
-setTimeout(() => {
-    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+$('.images > img:nth-child(3)').addClass('enter')
+let n = 1
+setInterval(() => {
+    if(n>3){
+        n = n%3
+        if (n===0){
+            n = 3
+        }
+    }
+    $(`.images > img:nth-child(${n})`).removeClass('current').addClass('leave')
         .one('transitionend', (e) => {
             $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-}, 2000)
-setTimeout(() => {
-    $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
-        .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
-}, 4000)
-setTimeout(() => {
-    $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
-        .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
-        })
-    $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
-}, 6000)
+    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    n += 1
+}, 3000)
+function x(n){
+    if(n>3){
+        n = n%3
+        if (n===0){
+            n = 3
+        }
+    }
+    return n
+}
+
+
+// setTimeout(() => {
+    // $('.images > img:nth-child(2)').removeClass('current').addClass('leave')
+        // .one('transitionend', (e) => {
+            // $(e.currentTarget).removeClass('leave').addClass('enter')
+        // })
+    // $('.images > img:nth-child(3)').removeClass('enter').addClass('current')
+// }, 4000)
+// setTimeout(() => {
+    // $('.images > img:nth-child(3)').removeClass('current').addClass('leave')
+        // .one('transitionend', (e) => {
+            // $(e.currentTarget).removeClass('leave').addClass('enter')
+        // })
+    // $('.images > img:nth-child(1)').removeClass('enter').addClass('current')
+// }, 6000)
+// setTimeout(() => {
+    // $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+        // .one('transitionend', (e) => {
+            // $(e.currentTarget).removeClass('leave').addClass('enter')
+        // })
+    // $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+// }, 8000)
 
 
 
