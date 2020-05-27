d44dc2b660033fd7bb473168d835719780466d96
lianghl0416 <594207356@qq.com>
2020-05-27 18:35:55 +0800

    函数封装 优化

--- day-13/main.js
+++ day-13/main.js
@@ -45,33 +45,54 @@
 // })
 // }, 8000)
 
+let n
+初始化()
 
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-let n = 1
 setInterval(() => {
-    if(n>3){
-        n = n%3
-        if (n===0){
-            n = 3
-        }
-    }
-    $(`.images > img:nth-child(${n})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
         .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+            makeEeter($(e.currentTarget))
         })
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+        makeCurrent(getImage(n+1))
     n += 1
 }, 3000)
-function x(n){
-    if(n>3){
-        n = n%3
-        if (n===0){
+
+
+
+
+
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
             n = 3
         }
     }
     return n
+}
+
+function 初始化() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    $node.removeClass('leave enter').addClass('current')
+    return $node
+}
+function makeLeave($node){
+    $node.removeClass('current enter').addClass('leave')
+    return $node
+}
+function makeEeter($node){
+    $node.removeClass('current leave').addClass('enter')
+    return $node
 }
 
 
