b16fa2114ceca1be1d4351da6eb6f4cb68f45c9a
lianghl0416 <594207356@qq.com>
2020-05-28 14:59:15 +0800

    用jquery方法实现阻止冒泡

--- day-14 点击别处关闭浮层/main.js
+++ day-14 点击别处关闭浮层/main.js
@@ -1,7 +1,32 @@
-clickMe.addEventListener('click', function(e){
-    popover.style.display = 'block'
-    e.stopPropagation()
-})
-document.addEventListener('click',function(){
-    popover.style.display = 'none'
+
+
+// clickMe.addEventListener('click', function () {
+    // popover.style.display = 'block'
+// })
+// wrapper.addEventListener('click', function (e) {
+    // e.stopPropagation()
+// })
+// document.addEventListener('click', function () {
+    // popover.style.display = 'none'
+// })
+
+//用jquery的方法来写：
+
+// $(clickMe).on('click', function () {
+    // $(popover).show()
+    // $(document).one('click', function (){
+        // $(popover).hide()
+    // })
+// })
+// $(wrapper).on('click', function (e){
+    // e.stopPropagation()
+// })
+// 
+
+//jqurey方法优化
+$(clickMe).on('click', function () {
+    $(popover).show()
+    $(document).one('click', function (){
+        $(popover).hide()
+    })
 })
