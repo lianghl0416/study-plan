abc2204ea8d4ebae415e58afe4918e1cc20891a8
Liang Hong Liang <594207356@qq.com>
2020-06-11 11:21:50 +0800

    js优化

--- day-13/main.js
+++ day-13/main.js
@@ -135,46 +135,55 @@
 let $buttons = $('#buttonWrapper > button')
 let $slides = $('#slides')
 let $images = $slides.children('img')
-let $firstCopy = $images.eq(0).clone(true)
-let $lastCopy = $images.eq($images.length - 1).clone(true)
-$slides.append($firstCopy)
-$slides.prepend($lastCopy)
-$slides.css({ transform: 'translate(-400px)' })
 let current = 0
 
-$buttons.eq(0).on('click', function () {
-    if (current == 2) {
-        $slides.css({ transform: 'translate(-1600px)' })
-            .one('transitionend', function () {
-                $slides.hide()
-                    .offset()
-                $slides.css({ transform: 'translateX(-400px)' })
-                    .show()
-            })
-    } else {
-        $slides.css({ transform: 'translateX(-400px)' })
-    }
-    current = 0
-})
-$buttons.eq(1).on('click', function () {
-    $slides.css({ transform: 'translateX(-800px)' })
-    current = 1
-})
-$buttons.eq(2).on('click', function () {
-    if (current == 0) {
-        $slides.css({ transform: 'translate(0px)' })
-            .one('transitionend', function () {
-                $slides.hide()
-                    .offset()
-                $slides.css({ transform: 'translateX(-1200px)' })
-                    .show()
-            })
-    } else {
-        $slides.css({ transform: 'translateX(-1200px)' })
-        current = 2
-    }
+makeFakeSlides()
 
-})
+$slides.css({ transform: 'translate(-400px)' })
+
+bindEvents()
+
+function bindEvents(){
+    $buttons.eq(0).on('click', function () {
+        if (current == 2) {
+            $slides.css({ transform: 'translate(-1600px)' })
+                .one('transitionend', function () {
+                    $slides.hide()
+                        .offset()
+                    $slides.css({ transform: 'translateX(-400px)' })
+                        .show()
+                })
+        } else {
+            $slides.css({ transform: 'translateX(-400px)' })
+        }
+        current = 0
+    })
+    $buttons.eq(1).on('click', function () {
+        $slides.css({ transform: 'translateX(-800px)' })
+        current = 1
+    })
+    $buttons.eq(2).on('click', function () {
+        if (current == 0) {
+            $slides.css({ transform: 'translate(0px)' })
+                .one('transitionend', function () {
+                    $slides.hide()
+                        .offset()
+                    $slides.css({ transform: 'translateX(-1200px)' })
+                        .show()
+                })
+        } else {
+            $slides.css({ transform: 'translateX(-1200px)' })
+            current = 2
+        }
+    })
+}
+
+function makeFakeSlides() {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
 
 
 
